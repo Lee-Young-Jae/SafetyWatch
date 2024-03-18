7afc28e85f196726efc93f6136f34e5231cf1834
Lee-Young-Jae <ori_@kakao.com>
2024-03-19 00:37:53 +0900

    refactor: 매직 넘버를 상수화한다.

---
+++ src/utills/constants.ts
@@ -0,0 +1,14 @@
+export const Casualty = {
+  INJURED_WEIGHT: 0.55,
+  CASUALTY_WEIGHT: 1.67,
+  SCORE: {
+    BAD_MAX: 8,
+    BAD_MIN: 6,
+    NORMAL_MAX: 5.99,
+    NORMAL_MIN: 3,
+    GOOD_MAX: 2.99,
+    GOOD_MIN: 0,
+  },
+} as const;
+
+export default Casualty;
