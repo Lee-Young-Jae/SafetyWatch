0fac973d76c74c1d75b64f8c219594fd121c6e18
Lee-Young-Jae <ori_@kakao.com>
2024-03-20 01:02:58 +0900

    style: 유저 인터페이스 작성

---
+++ src/pages/Search/Style.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 0 auto;
+  width: 80dvw;
+`;
+
+const InputField = styled.form`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  margin-top: 5rem;
+  margin-bottom: 20px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 40px;
+  padding: 0 10px;
+  border: 2px solid #3498db;
+  border-radius: 5px;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const SearchButton = styled.button`
+  width: 4rem;
+  height: 40px;
+  background-color: #3498db;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-left: 10px;
+`;
+
+const S = {
+  Container,
+  Input,
+  InputField,
+  SearchButton,
+};
+
+export default S;
