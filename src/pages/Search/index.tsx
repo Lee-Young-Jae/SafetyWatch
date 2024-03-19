9041048454cdb51c542b696c9e27471f080979a1
Lee-Young-Jae <ori_@kakao.com>
2024-03-20 01:03:54 +0900

    feat: 법령 검색 페이지 작성

---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import useInput from "../../hooks/useInput";
+import SearchResult from "./SearchField";
+import S from "./Style";
+
+const Search = () => {
+  const [search, setSearch] = useState("");
+
+  const { onChange, value } = useInput("");
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch(value);
+  };
+
+  return (
+    <S.Container>
+      <S.InputField id="search_form" onSubmit={onSubmit}>
+        <S.Input
+          type="text"
+          placeholder="검색어를 입력하세요"
+          value={value}
+          onChange={onChange}
+        />
+        <S.SearchButton type="submit">검색</S.SearchButton>
+      </S.InputField>
+      <SearchResult search={search} />
+    </S.Container>
+  );
+};
+
+export default Search;
