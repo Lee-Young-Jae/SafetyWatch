6c714eab60284ec10978e38726b5e31186afd176
Lee-Young-Jae <ori_@kakao.com>
2024-03-20 01:03:20 +0900

    chore: react-router-dom 초기설정 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Routes, Route } from "react-router-dom";
 
 import Main from "./pages/Main";
+import Search from "./pages/Search";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="search" element={<Search />}></Route>
+      </Routes>
       <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
